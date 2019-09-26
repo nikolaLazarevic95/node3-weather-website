b7e1492849c529dfb45d3e8741e052a718b0c26f
unknown <nikola.lazaa@gmail.com>
2019-09-26 04:53:52 +0200

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()     //!express je fja,ne obj mora da se call
+const port = process.env.PORT || 3000
 
 //Define paths for Exress config
 const publicDirectoryPath = path.join(__dirname, '../public') 
@@ -105,8 +106,8 @@ app.get('*', (req, res)=>{
     })
 })
 
-app.listen(3000, ()=> {
-    console.log('Server is up on port 3000.')
+app.listen(port, ()=> {
+    console.log('Server is up on port ' +port)
 })
 
 
